89731bb9603e35cf8ff1342eaeb374a74470f8e9
root <root@ubuntu.(none)>
2013-11-28 17:47:00 +0530

    Changed contact information table

--- app/models/contact_information.rb
+++ app/models/contact_information.rb
@@ -1,4 +1,5 @@
 class ContactInformation < ActiveRecord::Base
   attr_accessible :contact_name,:email,:phone_number,:fax,:pob,:website
   validates_presence_of :contact_name,:email,:phone_number,:fax,:pob,:website
+  belongs_to :organisation
 end

9411a7d49c2cc5415d404c6e22cc37d7f54b72c3
root <root@ubuntu.(none)>
2013-11-29 17:52:07 +0530

    amend

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 
 class Category < ActiveRecord::Base
    attr_accessible :type
+   has_many :subcategories
    has_many :organisations_categories
    has_many :organisations , :through => :organisations_categories
    validates_presence_of :type
